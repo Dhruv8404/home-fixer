14733b3ad1ccb48d74cba83543034d491b5d5fc9
pateldhruv8404-star <pateldhruv8404@gmail.com>
2026-02-16 18:30:52 +0530

    Next.js  setup

--- home_fixer/settings.py
+++ home_fixer/settings.py
@@ -58,6 +58,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework_simplejwt.token_blacklist',
     'home',
+    'corsheaders',
     'rest_framework',
     'drf_yasg',
 ]
@@ -66,6 +67,7 @@ AUTH_USER_MODEL = 'home.User'
 
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -74,6 +76,10 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
+
+
+CORS_ALLOW_ALL_ORIGINS = True
+
 
 ROOT_URLCONF = 'home_fixer.urls'
 
