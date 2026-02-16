64bf121b32ddc66f644436d57cf411a4d025d757
pateldhruv8404-star <pateldhruv8404@gmail.com>
2026-02-16 21:54:53 +0530

    Fix SECRET_KEY empty string issue for Docker build

--- home_fixer/settings.py
+++ home_fixer/settings.py
@@ -39,8 +39,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 
 
-SECRET_KEY = os.getenv("SECRET_KEY", "django-insecure-u%7rdl^!a2bz&u89$6eyzwp+_bo+-e0dgnt45p3ip2q*t7zdvy")
-
+SECRET_KEY = os.getenv("SECRET_KEY") or "django-insecure-u%7rdl^!a2bz&u89$6eyzwp+_bo+-e0dgnt45p3ip2q*t7zdvy"
 DEBUG = os.getenv("DEBUG", "False") == "True"
 
  
