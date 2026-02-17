e8a253df0ed477f1c360809bbc6fd86fdb03da3b
pateldhruv8404-star <pateldhruv8404@gmail.com>
2026-02-18 01:10:55 +0530

    Add Dockerfile for Render

--- home_fixer/settings.py
+++ home_fixer/settings.py
@@ -17,8 +17,10 @@ import dj_database_url
 load_dotenv()
 
 
-
-EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
+EMAIL_BACKEND = os.getenv(
+    "EMAIL_BACKEND",
+    "django.core.mail.backends.smtp.EmailBackend"
+)
 EMAIL_HOST = os.getenv("EMAIL_HOST")
 EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
 EMAIL_USE_TLS = True
