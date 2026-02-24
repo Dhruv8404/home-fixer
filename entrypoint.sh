20fd2aaa395e9695ab057a3ae72220642f077d9c
pateldhruv8404-star <pateldhruv8404@gmail.com>
2026-02-24 06:03:29 +0530

    Fix: remove gunicorn control file

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,6 @@
 #!/bin/sh
 
-echo "Starting Django..."
-
 python manage.py migrate --noinput
 python manage.py collectstatic --noinput
 
-gunicorn home_fixer.wsgi:application --bind 0.0.0.0:8000 --workers 3
+exec gunicorn home_fixer.wsgi:application --bind 0.0.0.0:$PORT
