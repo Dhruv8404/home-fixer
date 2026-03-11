834968858b4b4a4e070c3321a22281c38dc221aa
pateldhruv8404-star <pateldhruv8404@gmail.com>
2026-03-11 16:23:18 +0530

    booking

--- home_fixer/settings.py
+++ home_fixer/settings.py
@@ -100,7 +100,10 @@ MIDDLEWARE = [
 ]
 
 CORS_ALLOW_ALL_ORIGINS = True
-
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
 CORS_ALLOW_HEADERS = [
     "accept",
     "accept-encoding",
